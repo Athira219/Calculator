2f76043fef4953fe247713610625f8eebe6cc932
Athira Somaraj <athirasomaraj070gmail.com>
2022-07-21 14:25:08 +0530

    script added

---
+++ calc.js
@@ -0,0 +1,16 @@
+function buttonclick(val){
+
+    document.getElementById('screen').value=document.getElementById('screen').value+val;
+}
+function cleardisplay(){
+
+    document.getElementById('screen').value=""
+}
+
+function equalclick(){
+    
+    var text= document.getElementById('screen').value
+    var result=eval(text)
+    
+    document.getElementById('screen').value=result
+ }
